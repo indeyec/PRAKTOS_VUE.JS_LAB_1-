c1d4583c460e583366398d36ac18cfc0dfd355d7
gr413_shivol <vlad.shilov03@mail.ru>
2023-01-30 10:26:34 +0700

    zadanie_3_completed

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ let app = new Vue({
         link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
         inStock: true,
         inventory:100,
+        onSale: true,
     }
  })
  
