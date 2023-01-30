cf10f66e7c24d8fac0038c3da3b86773ce4d98d4
gr413_shivol <vlad.shilov03@mail.ru>
2023-01-30 11:18:54 +0700

    pr_zadanie_5_complited

--- js/main.js
+++ js/main.js
@@ -1,27 +1,36 @@
+ 
 let app = new Vue({
     el: '#app',
     data: {
-        product: "Socks",
-        description: "A pair of warm, fuzzy socks.",
-        image: "./assets/vmSocks-blue-onWhite.jpg",
-        altText: "A pair of socks",
-        link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
-        inStock: true,
-        inventory:100,
-        onSale: true,
-        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-        variants: [
-            {
-                variantId: 2234,
-                variantColor: 'green'
-            },
-            {
-                variantId: 2235,
-                variantColor: 'blue'
-            }
-         ],
-         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
-         
+      product: 'Socks',
+      image: "./assets/vmSocks-blue-onWhite.jpg",
+      altText: "A pair of socks",
+      link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
+      inStock: true,
+      details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+      variants: [
+        {
+          variantId: 2234,
+          variantColor: 'green',
+          variantImage: "./assets/vmSocks-green-onWhite.jpg"
+        },
+        {
+          variantId: 2235,
+          variantColor: 'blue',
+          variantImage: "./assets/vmSocks-blue-onWhite.jpg"
+        }
+      ],
+      cart: 0
+    },
+    methods: {
+      addToCart() {
+        this.cart += 1
+      },
+      updateProduct(variantImage) {
+        this.image = variantImage
+      },
+      removeFromCart() {
+        this.cart -= 1
+      }
     }
- })
- 
+  })
